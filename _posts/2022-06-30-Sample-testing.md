61a84496dd4eae6126fd4d3093b3da492a6d58fa
Uzzal Hossain <de3dm3nu11@gmail.com>
2022-06-30 23:35:58 +0600

    update post script

---
+++ _posts/2022-06-30-Sample-testing.md
@@ -0,0 +1,14 @@
+---
+title: "Sample testing" #gist test
+date: "2022-06-30 23:15:42 +06"
+last_modified_at: #2022-06-27 18:12:17 +6
+published: true
+header:
+    teaser: #/assets/images/my-awesome-post-teaser.jpg
+    og_image: #/assets/images/page-header-og-image.png
+# categories:
+#     - github
+
+tags:
+    - #plugins
+---
--- post.sh
+++ post.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
-template="---
-title: #gist test
-date: \"$((date +"%Y-%m-%d %H:%M:%S %z") | cut -c 1-23)\"
-last_modified_at: #2022-06-27 18:12:17 +6
-published: #false
-header:
-    teaser: #/assets/images/my-awesome-post-teaser.jpg
-    og_image: #/assets/images/page-header-og-image.png
-categories:
-    - #github
-
-tags:
-    - #plugins
----"
-now=$(date +"%Y-%m-%d")
 
 name=$1
 output_dir=$2
 fomatted_name="${name// /-}"
+now=$(date +"%Y-%m-%d")
+
+template="---
+title: \"$1\" #gist test
+date: \"$((date +"%Y-%m-%d %H:%M:%S %z") | cut -c 1-23)\"
+last_modified_at: #2022-06-27 18:12:17 +6
+published: true
+header:
+    teaser: #/assets/images/my-awesome-post-teaser.jpg
+    og_image: #/assets/images/page-header-og-image.png
+#categories:
+#    - github
+
+tags:
+    - #plugins
+---"
+
 if [ "$1" != "" ]; then
     if [ "$2" != "" ]; then
         if [ -d "$2" ]; then
             echo "Creating post $fomatted_name"
-            echo "$template" > "$output_dir/$fomatted_name.md"
+            echo "$template" > "$output_dir/$now-$fomatted_name.md"
             echo "Created post $fomatted_name"
         else
             echo "Output directory $2 does not exist"
